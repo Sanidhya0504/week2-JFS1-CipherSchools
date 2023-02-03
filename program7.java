74b6295847378fef75bd068caecb3108a0442761
Sanidhya <sharmasanidhya0504@gmail.com>
2023-02-03 19:10:53 +0530

    First Commit

---
+++ program7.java
@@ -0,0 +1,14 @@
+
+package week2;
+
+import java.io.File;
+import java.io.IOException;
+
+public class program7 {
+    public static void main(String[] args) throws IOException{
+        File f=new File("d:\\abc.txt");
+        f.createNewFile();
+        //throws is an indication that thee is a need of exception handling.
+        
+    }
+}
